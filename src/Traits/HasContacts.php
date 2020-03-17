262e25b8e40faa162d2f28b00753186cf06b0c55
Aguinaldo Tupy <44652991+aguinaldotupy@users.noreply.github.com>
2020-03-17 20:04:29 +0000

    Update HasContacts.php

--- src/Traits/HasContacts.php
+++ src/Traits/HasContacts.php
@@ -15,4 +15,51 @@ trait HasContacts
     {
         return $this->morphOne(Contact::class, 'contactable');
     }
+    
+    public function getMobileAttribute()
+    {
+        if ($contact = $this->contacts()->whereType('Celular')->first()) {
+            return $contact->value;
+        }
+
+        return null;
+    }
+
+    public function getEmailAttribute()
+    {
+        if ($contact = $this->contacts()->whereType('Email')->first()) {
+            return $contact->value;
+        }
+
+        return null;
+    }
+
+    public function getPhoneAttribute()
+    {
+        if ($contact = $this->contacts()->whereType('Telefone')->first()) {
+            return $contact->value;
+        }
+
+        return null;
+    }
+
+    public function setContact(string $type, string $value) : \Illuminate\Database\Eloquent\Model
+    {
+        return $this->contacts()->create([
+            'type' => $type,
+            'value' => $value,
+        ]);
+    }
+
+    public function updateContact(string $type, string $value)
+    {
+        if ($contact = $this->contacts()->whereType($type)->first()) {
+            $contact->value = $value;
+            $contact->update();
+
+            return $contact;
+        }
+
+        return null;
+    }
 }
